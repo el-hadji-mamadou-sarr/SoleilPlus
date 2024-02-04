33ae4643ae211162c231299edbf7517f33c5a396
el hadji mamadou sarr <elhadji.sarr@outlook.com>
2024-02-04 14:53:12 +0100

    splash feature

--- App.js
+++ App.js
@@ -36,6 +36,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#90d6fa",
     alignItems: "center",
     justifyContent: "center",
   },
